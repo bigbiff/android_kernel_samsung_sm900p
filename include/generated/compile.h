ef783309e93a1d66a21e8333f46aba1db05819d9
bigbiff bigbiff <bigbiff@teamw.in>
2013-10-18 20:57:53 -0400

    initial import

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Fri Oct 18 20:03:18 EDT 2013"
+#define LINUX_COMPILE_BY "bigbiff"
+#define LINUX_COMPILE_HOST "nas"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
